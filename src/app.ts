aaa5e336dd783bfd104b3ad6011cc870ae3ff96c
mind-ll-matter <aj.git001@gmail.com>
2023-06-09 09:45:26 +1200

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import helloWorldRoutes from './6-routes/helloWorldRoute'
 import listingsRoutes from './6-routes/listingsRoute'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json()) // lets our server know that the transactions are going to be in json. So when we get data, we want to treat it as json and parse it as json.
+app.use(cors())
 
 app.use('/api/listings', listingsRoutes)
 
